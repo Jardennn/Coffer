a28f63eb198c6594203ce97d5330926b7c6e3e66
Jardennn <177467025+Jardennn@users.noreply.github.com>
2026-08-01 17:56:15 +0300

    Initial commit of C# implementation

---
+++ Coffer.Core/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Coffer.Core
+{
+  class Program
+  {
+    static void Main(String[] args)
+    {
+      Console.WriteLine("Enter Source root.");
+      string SourceRoot = Console.ReadLine();
+      Console.WriteLine("Enter Destination root.");
+      string DestRoot = Console.ReadLine();
+
+      Console.WriteLine($"Source root - {SourceRoot}");
+      Console.WriteLine($"Destination root - {DestRoot}");
+    }
+  }
+}
---
+++ Coffer.Services/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Coffer.Services;
+
+public class Class1
+{
+
+}
