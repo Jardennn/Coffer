3439edd49064d546f66e7ae1fa7e0c50fcd70bc0
Jardennn <177467025+Jardennn@users.noreply.github.com>
2026-08-07 19:31:57 +0300

    Added filters and detectors (for having cross platform functionality)

--- Coffer.Core/Program.cs
+++ Coffer.Core/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Coffer.Services;
+using Coffer.Services.Models;
 
 namespace Coffer.Core
 {
@@ -10,8 +11,14 @@ namespace Coffer.Core
         {
 
             // Now instead of putting your absolute paths in an array you need to put your root source paths in an array and root destination path in a single string.
-             string[] srcpaths = ["/path/to/root/source/"];
-             string dst = "/path/to/root/destination/";
+            string[] srcpaths = ["/path/to/root/source/"];
+            string dst = "/path/to/root/destination/";
+
+            FilterConfig filters = new FilterConfig
+            {
+                // Custom filters go here in this format:
+                // ExcludeExtensions = new() {".ext"},
+            };
 
             Console.WriteLine("Starting data transfer.");
             for (int i = 0; i < srcpaths.Length; i++) // Going through every path in srcpaths
@@ -27,35 +34,36 @@ namespace Coffer.Core
 
                 foreach (var filepath in filepaths)
                 {
-                    string filepathstr = filepath.FullName;
-                    string dstpath = GetDest.GetDestPath(srcpath, filepathstr, dst);
-
-                    Directory.CreateDirectory(Path.GetDirectoryName(dstpath)!); // Creates a directory in the same name of a directory that contains a nested source file.
-
-                    Console.WriteLine($"Beginning data transfer for: {filepathstr}");
-                    string srchash = Copier.CopyFile(filepathstr, dstpath, 4, onProgress: (copied, total) => // Using lambda to pass onProgress as a function that will take 'copied' and 'total' as arguments.
+                    if (Scanner.ShouldInclude(filepath, filters))
                     {
-                        int percent = (int)((double)copied / total * 100); // Percentage calculation for how much of the file was copied.
-                        int filled = percent / 2; // Making the progress bar 50 characters wide
-                        string bar = new string('█', filled) + new string('░', 50 - filled); // Two strings are combined to make the progress bar, the filled blocks are in the same length as 'filled' that changes dynamically and the empty blocks are the full length of the bar (50) - the filled blocks (filled variable).
-                        Console.Write($"\r [{bar}] {percent}% {copied / 1024 / 1024}MB / {total / 1024 / 1024}MB"); // Output of the progress bar, \r moves the console back to the beginning of the line for updating the progress bar.
-                    });
-                    Console.WriteLine();
+                      string filepathstr = filepath.FullName;
+                      string dstpath = GetDest.GetDestPath(srcpath, filepathstr, dst);
 
-                    Console.WriteLine("Transfer complete, beginning hash verficiation.");
-                    bool verification = Verifier.Verify(srchash, dstpath, 4);
+                      Directory.CreateDirectory(Path.GetDirectoryName(dstpath)!); // Creates a directory in the same name of a directory that contains a nested source file.
 
-                    if (verification)
-                    {
+                      Console.WriteLine($"Beginning data transfer for: {filepathstr}");
+                      string srchash = Copier.CopyFile(filepathstr, dstpath, 4, onProgress: (copied, total) => // Using lambda to pass onProgress as a function that will take 'copied' and 'total' as arguments.
+                      {
+                          int percent = (int)Math.Clamp((double)copied / total * 100, 0, 100); // Percentage calculation for how much of the file was copied. Math.Clamp is for limiting the bar to be within 0 to 100, to prevent crashes.
+                          int filled = percent / 2; // Making the progress bar 50 characters wide
+                          string bar = new string('█', filled) + new string('░', 50 - filled); // Two strings are combined to make the progress bar, the filled blocks are in the same length as 'filled' that changes dynamically and the empty blocks are the full length of the bar (50) - the filled blocks (filled variable).
+                          Console.Write($"\r [{bar}] {percent}% {copied / 1024 / 1024}MB / {total / 1024 / 1024}MB"); // Output of the progress bar, \r moves the console back to the beginning of the line for updating the progress bar.
+                      });
+                      Console.WriteLine();
+
+                      Console.WriteLine("Transfer complete, beginning hash verficiation.");
+                      bool verification = Verifier.Verify(srchash, dstpath, 4);
+
+                      if (verification)
+                      {
                         Console.WriteLine("Verify complete.");
+                      }
+                      else
+                         Console.WriteLine("Verification failed.");
+
+                      Console.Write("\n--------------------------------\n");
                     }
-                    else
-                        Console.WriteLine("Verification failed.");
-
-                    Console.Write("\n--------------------------------\n");
                 }
-
-
             }
             Console.WriteLine("Backup complete.");
         }
--- Coffer.Services/Services/Scanner.cs
+++ Coffer.Services/Services/Scanner.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Collections.Generic;
+using Coffer.Services.Models;
 
 namespace Coffer.Services
 {
@@ -15,6 +16,117 @@ namespace Coffer.Services
                 results.Add(file);
             }
             return results;
+        }
+
+
+
+        public static bool ShouldInclude(FileInfo file, FilterConfig filters)
+        {
+            if (filters.ExcludeExtensions.Contains(file.Extension.ToLower())) // Extension exclusion filtering
+            {
+                return false;
+            }
+
+            if (filters.IncludeExtensions != null && !filters.IncludeExtensions.Contains(file.Extension.ToLower())) // Extension inclusion filtering
+            {
+                return false;
+            }
+
+            if (filters.MaxSizeMB.HasValue && file.Length > filters.MaxSizeMB * 1024 * 1024) // File size limiation in MB
+            {
+                return false;
+            }
+
+            if (file.Length < filters.MinSizeMB * 1024 * 1024) // File size filtering by minimum MBs
+            {
+                return false;
+            }
+
+            foreach (var excludedFolder in filters.ExcludeFolders) // Folder filtering (Checking if the given folder is in the full path of the given file)
+            {
+                if (file.FullName.Contains(Path.DirectorySeparatorChar + excludedFolder + Path.DirectorySeparatorChar))
+                {
+                    return false;
+                }
+            }
+
+            foreach (var excludedPath in filters.ExcludePaths)
+            {
+                if (file.FullName.StartsWith(excludedPath))
+                {
+                    return false;
+                }
+            }
+
+            if (filters.SkipHidden && Detectors.HiddenDetect(file)) // Hidden files filtering (Checking if SkipHidden is set to true and using the hidden detection funtion for detecting if the path is hidden.)
+            {
+                return false;
+            }
+
+            if (filters.SkipReadOnly && file.IsReadOnly)
+            {
+                return false;
+            }
+
+            if (filters.SkipSystemFiles && Detectors.SystemFileDetect(file))
+            {
+                return false;
+            }
+
+            if (!filters.FollowSymlinks && file.LinkTarget != null)
+            {
+                return false;
+            }
+
+            if (!filters.FollowSymlinks && Detectors.SymlinkParentDetect(file))
+            {
+                return false;
+            }
+
+            // If file last modification date is before the filter date, it will exclude it.
+            if (filters.ModifiedAfter.HasValue && filters.ModifiedAfter.Value > file.LastWriteTime)
+            {
+                return false;
+            }
+
+            // If file modification date is after the filter date, it will exclude it.
+            if (filters.ModifiedBefore.HasValue && filters.ModifiedBefore.Value < file.LastWriteTime)
+            {
+                return false;
+            }
+
+            if (filters.CreatedAfter.HasValue && filters.CreatedAfter.Value > file.CreationTime)
+            {
+                return false;
+            }
+
+            if (filters.CreatedBefore.HasValue && filters.CreatedBefore.Value < file.CreationTime)
+            {
+                return false;
+            }
+
+            if (filters.ModifiedWithinDays.HasValue)
+            {
+              DateTime Cutoff = DateTime.Now.AddDays(-filters.ModifiedWithinDays.Value);
+
+              if (file.LastWriteTime < Cutoff)
+              {
+                    return false;
+              }
+            }
+
+            if (filters.CreatedWithinDays.HasValue)
+            {
+              DateTime Cutoff = DateTime.Now.AddDays(-filters.CreatedWithinDays.Value);
+
+              if (file.CreationTime < Cutoff)
+              {
+                  return false;
+              }
+            }
+
+            return true;
+
         }
     }
 }
